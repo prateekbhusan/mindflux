0c117a31edc67107458a8cd9a008998bdce0d896
prateekbhusan <pbhusan23@gmail.com>
2025-10-05 11:49:29 +0530

    fix: Complete overhaul for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import { createRequestHandler } from '@remix-run/node';
-import * as build from '../build/index.js';
+import { createRequestHandler } from "@remix-run/vercel";
+import * as build from "@remix-run/dev/server-build";
 
-export default createRequestHandler(build, process.env.NODE_ENV);
+export default createRequestHandler({ build, mode: process.env.NODE_ENV });
