6a4d922b8a063bd66cb9b8efb66848c08a67b0fd
prateekbhusan <pbhusan23@gmail.com>
2025-10-05 10:15:35 +0530

    fix: Switch to Vercel/Node.js server entry

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,7 @@
-import { isbot } from "isbot"
-import { renderToReadableStream } from "react-dom/server"
-import type { AppLoadContext, EntryContext } from "react-router"
-import { ServerRouter } from "react-router"
+import {
+  createReadableStreamFromReadable,
+  handleRequest,
+} from '@remix-run/node';
 
 export default async function handleRequest(
     request: Request,
